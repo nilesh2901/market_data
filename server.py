b6c00da683f3dcc232c913c413ab65ed00fcd7ae
Nilesh Ananthanarayanan <nilesh.ananth@gmail.com>
2026-03-07 03:28:10 -0500

    fix: explicit directory structure for Render

--- server.py
+++ server.py
@@ -132,3 +132,4 @@ if __name__ == '__main__':
     # Use the port assigned by Render, or default to 5000 for local testing
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
